280707d4ff087a871bc97a5b89902a3adf678b31
Vuthipong Chureeganon <thecvch@hotmail.com>
2012-09-25 03:13:17 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTube::Application.config.secret_token = '8016b86a0d83e71843dab5a62bd82dd7df440a8ee3f2e6dc18c254fab82d808541fc027c71cf0a6426b04db564eb5dc04dbafaadc26de9553ac668ae40ad0c8a'
